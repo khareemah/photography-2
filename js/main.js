fc1bfdc9445bbbaee1ec04759b03bfa5ee0bb676
khareemah <ajimobikareemah@gmail.com>
2020-10-31 03:17:31 -0700

    add lightbox, finally, i'm through with the MF project

--- js/main.js
+++ js/main.js
@@ -3,6 +3,57 @@ const navToggle = document.querySelector(".nav-toggle");
 const nav = document.querySelector(".nav");
 const loader = document.querySelector(".loader");
 const ul = document.querySelector(".nav ul");
+const lightbox = document.querySelector(".lightbox");
+const lightboxImg = document.querySelector(".lightbox-img");
+const allImages = document.querySelectorAll(".work-item-inner img");
+const workItems = document.querySelectorAll(".works .work-item-inner");
+const closeBtn = document.querySelector(".close-btn");
+const captionText = document.querySelector(".lightbox-caption");
+const numText = document.querySelector(".lightbox-number");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+
+for (let i = 0; i < workItems.length; i++) {
+  workItems[i].addEventListener("click", function() {
+    openModal();
+    let imgSrc = this.querySelector("img").getAttribute("data-large");
+    let caption = this.querySelector("img").alt;
+    let currentlyselected = i;
+    lightboxImg.src = imgSrc;
+    captionText.innerHTML = caption;
+    numText.innerHTML = `${currentlyselected + 1} / 6`;
+    nextBtn.addEventListener("click", function() {
+      currentlyselected++;
+      if (currentlyselected === allImages.length) currentlyselected = 0;
+      lightboxImg.src = allImages[currentlyselected].getAttribute("data-large");
+      captionText.innerHTML = allImages[currentlyselected].alt;
+      numText.innerHTML = `${currentlyselected + 1} / 6`;
+    });
+    prevBtn.addEventListener("click", function() {
+      if (currentlyselected === 0) currentlyselected = allImages.length;
+      currentlyselected--;
+      lightboxImg.src = allImages[currentlyselected].getAttribute("data-large");
+      captionText.innerHTML = allImages[currentlyselected].alt;
+      numText.innerHTML = `${currentlyselected + 1} / 6`;
+    });
+  });
+}
+closeBtn.addEventListener("click", function() {
+  closeModal();
+});
+lightbox.addEventListener("click", function(e) {
+  if (e.target.classList.contains("lightbox")) {
+    closeModal();
+  }
+});
+
+function openModal() {
+  lightbox.style.display = "flex";
+}
+function closeModal() {
+  lightbox.style.display = "none";
+}
+// fixed header
 window.addEventListener("scroll", function() {
   if (this.window.scrollY > 100) {
     header.classList.add("fixed");
@@ -10,9 +61,13 @@ window.addEventListener("scroll", function() {
     header.classList.remove("fixed");
   }
 });
+
+// remove loader page when page loads
 window.addEventListener("load", function() {
   loader.classList.add("loaded");
 });
+
+// nav toggle
 navToggle.addEventListener("click", function() {
   nav.classList.toggle("open");
   navToggle.classList.toggle("open");
